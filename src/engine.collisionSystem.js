c3a3ad7483ccb53b8eb653a659ebf0931ec1a993
mike <mchkafka@gmail.com>
2020-11-24 17:41:21 +0100

    added collision system jsdoc

--- src/engine.collisionSystem.js
+++ src/engine.collisionSystem.js
@@ -1,17 +1,36 @@
-const { engineResources } = require("./engine.resources");
+const { BoxCollider } = require("./engine.boxCollider");
 
-const { GameObject } = require("./engine.gameObject");
-
-const CollisionSystem = {
+/**
+ * Collision system prototype, can be used to check
+ * collisions between box colliders
+ */
+class CollisionSystem {
   /**
-   * @type {import("./engine.boxCollider").BoxCollider[]}
+   * Creates collision system instance
    */
-  colliders: [],
+  constructor() {
+    /**
+     * Array of box colliders taking part in collision verification
+     * @type {BoxCollider[]}
+     */
+    this.colliders = [];
+  }
 
+  /**
+   * Removes all colliders from collision system
+   * @returns {CollisionSystem} self reference for easier method chaining
+   */
   reset() {
     this.colliders = [];
-  },
+    return this;
+  }
 
+  /**
+   * Checks if collision between colliders in system occurred, if so
+   * colliders onCollision methods are fired (collider ID is provided
+   * as first parameter)
+   * @returns {CollisionSystem} self reference for easier method chaining
+   */
   checkCollisions() {
     for (let i = 0; i < this.colliders.length; i += 1) {
       const collider = this.colliders[i];
@@ -28,7 +47,37 @@ const CollisionSystem = {
         }
       }
     }
-  },
-};
+    return this;
+  }
+}
 
-exports.CollisionSystem = CollisionSystem;
+// const CollisionSystem = {
+//   /**
+//    * @type {BoxCollider[]}
+//    */
+//   colliders: [],
+
+//   reset() {
+//     this.colliders = [];
+//   },
+
+//   checkCollisions() {
+//     for (let i = 0; i < this.colliders.length; i += 1) {
+//       const collider = this.colliders[i];
+
+//       for (let j = i + 1; j < this.colliders.length; j++) {
+//         const nextCollider = this.colliders[j];
+//         const colliderID = collider.doesCollide(nextCollider);
+
+//         // if collision ocurred fire collision functions for both colliders
+//         // taking part in collision
+//         if (colliderID !== undefined) {
+//           collider.onCollision(colliderID);
+//           nextCollider.onCollision(collider.id);
+//         }
+//       }
+//     }
+//   },
+// };
+
+exports.CollisionSystem = new CollisionSystem();
