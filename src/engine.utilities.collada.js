a5d39f030889d375957e327dfde9bd1950b7ea98
mike <mchkafka@gmail.com>
2020-11-25 15:12:10 +0100

    added collada utilites jsdoc

--- src/engine.utilities.collada.js
+++ src/engine.utilities.collada.js
@@ -4,9 +4,10 @@ const { Vector4 } = require("./engine.math.vector4");
 const { Vector3 } = require("./engine.math.vector3");
 
 /**
- *
- * @param {HTMLCollection} sourceElements
- * @param {*} name
+ * Allows to find source element by name in XML collada file
+ * @param {HTMLCollection} sourceElements source elements in XML collada file
+ * @param {string} name name of source element that should be found
+ * @returns {HTMLElement|undefined} source element representing array of values stored in XML collada file or undefined if element cannot be found
  */
 const findSourceElement = (sourceElements, name) => {
   for (let element of sourceElements) {
@@ -20,7 +21,9 @@ const findSourceElement = (sourceElements, name) => {
 };
 
 /**
- * @param {HTMLElement} sourceElement
+ * Acquires useful data from source element e.g. array of vertex positions, normals etc.
+ * @param {HTMLElement} sourceElement source element representing array of values stored in XML collada file
+ * @returns {Vector3[]|Vector4[]} set of vectors where each represents elements like position, normal vector, uv coordinates etc.
  */
 const parseSourceElement = (sourceElement, type = "position") => {
   const floatArrayText = sourceElement.getElementsByTagName("float_array")[0]
@@ -83,9 +86,12 @@ const parseSourceElement = (sourceElement, type = "position") => {
 };
 
 /**
- *
- * @param {HTMLCollection} inputElements
- * @param {String} inputSemantic
+ * Allows to acquire number that represents index in vertex info array
+ * for source give, single record in vertex info array might look like [1, 6, 3],
+ * offset 1 of source "position" means that sixth position is part of this vertex info
+ * @param {HTMLCollection} inputElements collection of input elements stored in XML collada file
+ * @param {string} inputSemantic name of input that source offset should be found e.g. POSITION
+ * @returns {number|undefined} source offset or undefined if such source is not found
  */
 const findInputOffset = (inputElements, inputSemantic) => {
   for (let element of inputElements) {
@@ -98,8 +104,18 @@ const findInputOffset = (inputElements, inputSemantic) => {
 };
 
 /**
- *
- * @param {HTMLElement} trianglesElement
+ * @typedef {Object} TrianglesParseResult
+ * @property {number} indexOffset offset in vertex info array for vertex (position) source
+ * @property {number} normalOffset offset in vertex info array for normal source
+ * @property {number} mapOffset offset in vertex info array for map source
+ * @property {number} colorOffset offset in vertex info array for color source
+ * @property {number[][]} vertices vertex info array
+ */
+
+/**
+ * Acquires vertex info array from given triangles element and offset for each source of vertex data
+ * @param {HTMLElement} trianglesElement XML element of collada file that represents mesh triangles (vertex info array)
+ * @returns {TrianglesParseResult} set of source offsets and vertex info array
  */
 const parseTrianglesElement = (trianglesElement) => {
   const pElementText = trianglesElement.getElementsByTagName("p")[0]
@@ -128,10 +144,11 @@ const parseTrianglesElement = (trianglesElement) => {
 };
 
 /**
- * @param {XMLDocument} xml
- * @param {HTMLElement} geometry
+ * Reads given XML geometry tag from collada file that contains single mesh object definition
+ * @param {HTMLElement} geometry XML geometry tag
+ * @returns {Mesh} mesh object containing useful vertex data
  */
-const readGeometry = (xml, geometry) => {
+const readGeometry = (geometry) => {
   const geometryName = geometry.getAttribute("name");
   const meshElement = geometry.getElementsByTagName("mesh")[0];
   const sourceElements = meshElement.getElementsByTagName("source");
@@ -166,6 +183,11 @@ const readGeometry = (xml, geometry) => {
   return mesh;
 };
 
+/**
+ * Parses collada file into array of mesh objects that can be used as components of game objects
+ * @param {string} path path to collada file
+ * @returns {Mesh[]} array of meshes stored in collada file
+ */
 const readColladaFile = (path) => {
   const file = EngineToolbox.readTextFile(path);
   if (!file) {
@@ -180,7 +202,7 @@ const readColladaFile = (path) => {
   const meshes = [];
 
   for (let geometry of geometries) {
-    const mesh = readGeometry(xml, geometry);
+    const mesh = readGeometry(geometry);
     mesh.flipUV();
     meshes.push(mesh);
   }
